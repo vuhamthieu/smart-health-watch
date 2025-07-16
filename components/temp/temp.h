7b941a186e099114ccfd5b82d303938c1f08f07f
vuhamthieu <vuhamthieu@gmail.com>
2025-07-16 23:17:51 +0700

    temperature function

--- components/temp/temp.h
+++ components/temp/temp.h
@@ -1,15 +1,8 @@
+// components/temp/temp.h
 #ifndef _TEMP_H_
 #define _TEMP_H_
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void temp_init(void);
 float temp_read_celsius(void);
 
-#ifdef __cplusplus
-}
 #endif
-
-#endif // _TEMP_H_
