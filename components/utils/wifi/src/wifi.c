e092513f9da1526f517f5c09c1e6e593e3a5cee5
vuhamthieu <vuhamthieu@gmail.com>
2025-08-04 22:28:37 +0700

    adjust wifi issues

--- components/utils/wifi/src/wifi.c
+++ components/utils/wifi/src/wifi.c
@@ -30,7 +30,7 @@ bool is_wifi_connect_failed(void) {
 
 static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-            s_retry_num = 0;                // <-- THÊM DÒNG NÀY
+        s_retry_num = 0;               
         wifi_connect_failed = false; 
         esp_wifi_connect();
         isWifiConnecting = true;
